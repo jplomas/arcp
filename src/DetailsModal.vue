85dfc720bc84cf54db76ccea320b918cf98f88dc
JP Lomas <jp@theqrl.org>
2024-07-16 16:24:17 +0100

    Re-add ACCS update details

--- src/DetailsModal.vue
+++ src/DetailsModal.vue
@@ -19,6 +19,8 @@
           <dl>
             <dt>16th July 2024</dt>
             <dd>Form R links changed and TIS information added (thanks to Dr P Wilson for flagging!)</dd>
+            <dt>21st March 2024</dt>
+            <dd>ACCS years one and two details added.</dd>
             <dt>5th July 2023</dt>
             <dd>LTFT info for 2021 curriculum added.</dd>
             <dt>30th June 2023</dt>
